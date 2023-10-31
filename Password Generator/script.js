c8277851a5a2889ee21d4ef129ebfeeff9e7fb55
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-01 00:14:25 +0600

    PasswordGenerator class defined

---
+++ Password Generator/script.js
@@ -0,0 +1,10 @@
+// Create a js program that capable of generating a password which contains atleast one lowercase, one uppercase and one special characters. Create a password class to achieve the same
+
+class PasswordGenerator{
+    constructor(){
+        console.log("welcome to password generator");
+        this.pass = "";
+    }
+}
+
+// console.log(randPass.generatePassword(7,3));
