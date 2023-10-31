e91980a57ca07f1ef7eb6a24f6ed04b9b9a10c95
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-01 00:15:36 +0600

    generatePassword method defined

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -5,6 +5,24 @@ class PasswordGenerator{
         console.log("welcome to password generator");
         this.pass = "";
     }
+    generatePassword(len,difficulties){
+        let alphabets = "abcdefghijklmnopqrstuvwxyz";
+        let numbers = "1234567890";
+        let specialChar = "!@@#$%^&*";
+
+        if(len<3){
+            document.getElementById("alert").innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Please choose a valid input
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>`;
+        } 
+        
+    }
+    
 }
+let randPass = new PasswordGenerator();
+let randPass2 = new PasswordGenerator();
+let randPass3 = new PasswordGenerator();
+
 
 // console.log(randPass.generatePassword(7,3));
